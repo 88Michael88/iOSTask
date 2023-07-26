f8dca51d99aedcef4245e7c7ccbfe27e3fa89714
Michael <Moses>
2023-07-26 10:09:42 +0200

    Organize the project.

---
+++ EmpikTestApp/Model/WeatherManager.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherManager.swift
+//  EmpikTestApp
+//
+//  Created by Michael on 7/26/23.
+//
+
+import Foundation
+
+
+// API Key: dc6ae0d32c6a5e570873504f1d755c4c
+struct WeatherManager {
+    
+}
