eae5d5e4d45ac7fb3163654758dcbf52e6509b7e
Michael <Moses>
2023-07-26 11:08:05 +0200

    Make the IBActions and IBOutlets.

--- EmpikTestApp/Controller/SearchViewController.swift
+++ EmpikTestApp/Controller/SearchViewController.swift
@@ -9,21 +9,40 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var resultsTableView: UITableView!
+    
+    let temporaryResults = ["Krakow", "Warsaw"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        resultsTableView.delegate = self
+        resultsTableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func getLocationPressed(_ sender: UIBarButtonItem) {
     }
-    */
+}
 
+// MARK: - UITableViewDelegate
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        performSegue(withIdentifier: K.segueIdentifier , sender: self)
+    }
+}
+
+// MARK: - UITableViewDataSourch
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return temporaryResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIndetifier, for: indexPath)
+        cell.textLabel?.text = temporaryResults[indexPath.row]
+        return cell
+    }
+    
 }
---
+++ EmpikTestApp/Model/K.swift
@@ -0,0 +1,13 @@
+//
+//  Constances.swift
+//  EmpikTestApp
+//
+//  Created by Michael on 7/26/23.
+//
+
+import Foundation
+
+struct K {
+    static let cellIndetifier = "resultCell"
+    static let segueIdentifier = "weatherView"
+}
