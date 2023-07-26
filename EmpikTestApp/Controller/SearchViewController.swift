892d1a6c47d5bbe88070efee79638ca66a1c128c
Michael <Moses>
2023-07-26 12:57:05 +0200

    Implement searchTextField in SearchViewController.

--- EmpikTestApp/Controller/SearchViewController.swift
+++ EmpikTestApp/Controller/SearchViewController.swift
@@ -12,17 +12,27 @@ class SearchViewController: UIViewController {
     @IBOutlet var searchTextField: UITextField!
     @IBOutlet var resultsTableView: UITableView!
     
-    let temporaryResults = ["Krakow", "Warsaw"]
+    let resultsArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         resultsTableView.delegate = self
         resultsTableView.dataSource = self
+        searchTextField.delegate = self
+        searchTextField.addTarget(self, action: #selector(SearchViewController.textFieldDidChange(_:)), for: .editingChanged)
     }
     
     @IBAction func getLocationPressed(_ sender: UIBarButtonItem) {
     }
+}
+
+// MARK: - UITextFieldDelegate
+extension SearchViewController: UITextFieldDelegate {
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        resultsTableView.reloadData()
+    }
+    
 }
 
 // MARK: - UITableViewDelegate
@@ -30,18 +40,23 @@ extension SearchViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         performSegue(withIdentifier: K.segueIdentifier , sender: self)
+        print(indexPath.row)
     }
 }
 
 // MARK: - UITableViewDataSourch
 extension SearchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return temporaryResults.count
+        return resultsArray.count+1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIndetifier, for: indexPath)
-        cell.textLabel?.text = temporaryResults[indexPath.row]
+        if indexPath.row == 0 {
+            cell.textLabel?.text = "'\(searchTextField.text!)'"
+        }else {
+            cell.textLabel?.text = resultsArray[indexPath.row-1]
+        }
         return cell
     }
     
